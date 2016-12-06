fcb2c68ded105c5073e7b0345565e4f5ce187036
xieminjie <xieminjiexmj@gmail.com>
2016-12-07 08:03:11 +1100

    This is first CML git commit

---
+++ app/components/Examples.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var Nav = require('Nav');
+var Examples = (props) =>{
+	return(
+			<div>
+				<h2>Examples Component</h2>
+			</div>
+		);
+}
+
+module.exports = Examples;
---
+++ arrowFunction.js
@@ -0,0 +1,2 @@
+var add = (a,b) => a+b;
+console.log(add(1,2))
